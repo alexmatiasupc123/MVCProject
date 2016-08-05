13832c2cf6fe9323fc5076c5f26ff49599c12f02
Alexander Matias Soto <alexander.matias@gym.com.pe>
2016-08-05 16:22:11 -0500

    Arquitectura v1 + Footer

---
+++ MVCBolita.Entidad/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCBolita.Entidad
+{
+    public class Class1
+    {
+    }
+}
